d82b928c0b78e1b324625a9204ad95868e8ad209
Booth <ugrit.booth@gmail.com>
2024-03-30 13:05:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const fetch = require("node-fetch");
 const app = express();
 const port = process.env.PORT || 3000;
 const uid = new ShortUniqueId();
-const svaddr = "https://surl-qr-back-2.onrender.com";
-
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://s-url-qr-front-ki6ytygw2-boothza001s-projects.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(cors());
 app.use(bodyParser.json());
 
