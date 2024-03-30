fb75aa6b9367ce0a8fc97585dcc249d0a895b4ba
Booth <ugrit.booth@gmail.com>
2024-03-30 13:32:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const uid = new ShortUniqueId();
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://s-url-qr-front-ki6ytygw2-boothza001s-projects.vercel.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
