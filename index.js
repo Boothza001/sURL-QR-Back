495b1145e8323cc4a207d6932895133e9f860841
Booth <ugrit.booth@gmail.com>
2024-03-30 11:38:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const ShortUniqueId = require("short-unique-id");
 const cors = require("cors");
 const fetch = require("node-fetch");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const uid = new ShortUniqueId();
+const svaddr = "https://surl-qr-back-2.onrender.com";
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -22,21 +23,9 @@ const Url = mongoose.model("Url", {
   count: { type: Number, default: 0 },
 });
 
-// app.get("/api/redirect/:surl", async (req, res) => {
-//   const urlData = await Url.findOneAndUpdate(
-//     { surl: req.params.url },
-//     { $inc: { count: 1 } }
-//   );
-//   const surl3000 = urlData.surl;
-//   const url3000 = urlData.url;
-//   console.log(url3000);
-//   console.log(surl3000);
-//   res.redirect(url3000);
-// });
-
 app.get("/api/redirect/:surl", async (req, res) => {
   const urlData = await Url.findOneAndUpdate(
-    { surl: req.params.surl }, // แก้เป็น req.params.surl
+    { surl: req.params.surl },
     { $inc: { count: 1 } }
   );
   if (urlData) {
@@ -49,17 +38,6 @@ app.get("/api/redirect/:surl", async (req, res) => {
     res.status(404).send("URL not found");
   }
 });
-
-// app.get("/api/redirect/:surl", async (req, res) => {
-//   try {
-//     const shortUrl = req.params.surl;
-//     await Url.findOneAndUpdate({ surl: shortUrl }, { $inc: { count: 1 } });
-//     const urlData = await Url.findOne({ surl: shortUrl });
-//     res.redirect(urlData.url);
-//   } catch (error) {
-//     res.status(500).send(error);
-//   }
-// });
 
 app.post("/api/create", async (req, res) => {
   try {
@@ -94,3 +72,93 @@ app.delete("/api/delete/:id", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const ShortUniqueId = require("short-unique-id");
+// const fetch = require("node-fetch");
+// const app = express();
+// const uid = new ShortUniqueId();
+// const cors = require("cors");
+// const port = process.env.PORT || 3000;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+// const svaddr = "https://surl-qr-back-2.onrender.com";
+
+// mongoose
+//   .connect("mongodb+srv://root:1234@cluster0.sbjr9av.mongodb.net/db")
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+// const Url = mongoose.model("Url", {
+//   url: String,
+//   surl: String,
+//   domain: String,
+//   date: String,
+//   count: { type: Number, default: 0 },
+// });
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", svaddr);
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Server, Date");
+//   next();
+// });
+
+// app.get("/api/redirect/:surl", async (req, res) => {
+//   try {
+//     const urlData = await Url.findOneAndUpdate(
+//       { surl: req.params.surl },
+//       { $inc: { count: 1 } }
+//     );
+
+//     if (urlData) {
+//       const url3000 = urlData.url;
+//       res.redirect(url3000);
+//     } else {
+//       res.status(404).send("URL not found");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
+
+// app.post("/api/create", async (req, res) => {
+//   try {
+//     const { url } = req.body;
+//     const sUrl = uid.rnd(5);
+//     const domain = req.get("Server");
+//     const date = new Date();
+//     const newUrl = await Url.create({ url, surl: sUrl, domain, date });
+//     console.log("New URL data:", newUrl);
+//     console.log(newUrl);
+//     res.send(newUrl);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+
+// app.get("/api/show", async (req, res) => {
+//   try {
+//     const urls = await Url.find();
+//     res.send(urls);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+
+// app.delete("/api/delete/:id", async (req, res) => {
+//   try {
+//     const deletedUrl = await Url.findByIdAndDelete(req.params.id);
+//     !deletedUrl ? res.status(404).send("Url not found") : res.send(deletedUrl);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
