7c4f6ca21e13aa95874251599ba960cd49df47fa
Booth <ugrit.booth@gmail.com>
2024-03-30 08:26:07 +0700

    fix super bug

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ShortUniqueId = require("short-unique-id");
+const cors = require("cors");
+const fetch = require("node-fetch");
 const app = express();
 const port = 3000;
 const uid = new ShortUniqueId();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose
@@ -18,26 +22,44 @@ const Url = mongoose.model("Url", {
   count: { type: Number, default: 0 },
 });
 
+// app.get("/api/redirect/:surl", async (req, res) => {
+//   const urlData = await Url.findOneAndUpdate(
+//     { surl: req.params.url },
+//     { $inc: { count: 1 } }
+//   );
+//   const surl3000 = urlData.surl;
+//   const url3000 = urlData.url;
+//   console.log(url3000);
+//   console.log(surl3000);
+//   res.redirect(url3000);
+// });
+
 app.get("/api/redirect/:surl", async (req, res) => {
-  try {
-    const urlData = await Url.findOneAndUpdate(
-      { surl: req.params.surl },
-      { $inc: { count: 1 } }
-    );
-    if (urlData) {
-      const surl3000 = urlData.surl;
-      const url3000 = urlData.url;
-      console.log("URL ", url3000);
-      console.log("sURL ", surl3000);
-      res.redirect(url3000);
-    } else {
-      res.status(404).send("URL not found");
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+  const urlData = await Url.findOneAndUpdate(
+    { surl: req.params.surl }, // แก้เป็น req.params.surl
+    { $inc: { count: 1 } }
+  );
+  if (urlData) {
+    const surl3000 = urlData.surl;
+    const url3000 = urlData.url;
+    console.log(url3000);
+    console.log(surl3000);
+    res.redirect(url3000);
+  } else {
+    res.status(404).send("URL not found");
   }
 });
+
+// app.get("/api/redirect/:surl", async (req, res) => {
+//   try {
+//     const shortUrl = req.params.surl;
+//     await Url.findOneAndUpdate({ surl: shortUrl }, { $inc: { count: 1 } });
+//     const urlData = await Url.findOne({ surl: shortUrl });
+//     res.redirect(urlData.url);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
 
 app.post("/api/create", async (req, res) => {
   try {
