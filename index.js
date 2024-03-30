450e2e197990a6335eb6ca7545ff1e3eca5f5a1a
Booth <ugrit.booth@gmail.com>
2024-03-30 16:14:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 mongoose
   .connect("mongodb+srv://root:1234@cluster0.sbjr9av.mongodb.net/db")
   .then(() => console.log("Connected to MongoDB"))
