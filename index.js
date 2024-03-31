69ebff9ffeea6712dc9e82f0789cbf23ce4164c9
Booth <ugrit.booth@gmail.com>
2024-03-31 08:26:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ShortUniqueId = require("short-unique-id");
 const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 3000;
 const uid = new ShortUniqueId();
-app.use(cors());
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
